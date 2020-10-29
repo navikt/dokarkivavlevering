ea7b5019e7194f5195195fc7216b2392f510c310
Kim Andre Pollvik <kim.pollvik@nav.no>
2020-10-29 19:58:41 +0100

    MMA-4860: Initial commit

---
+++ app/src/main/java/no/nav/dokarkivavlevering/Application.java
@@ -0,0 +1,24 @@
+package no.nav.dokarkivavlevering;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+
+@Slf4j
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+        log.info("Dokarkivavlevering avslutter...");
+        context.close();
+    }
+
+    @Override
+    public void run(String... args) {
+        log.info("Dokarkivavlevering kjører...");
+    }
+}
