ecd3e5e9505790df5cfac12fa97797dcc972582b
Kim Andre Pollvik <kim.pollvik@nav.no>
2020-11-03 08:45:19 +0100

    MMA-4860

--- app/src/main/java/no/nav/dokarkivavlevering/Application.java
+++ app/src/main/java/no/nav/dokarkivavlevering/Application.java
@@ -5,9 +5,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Import;
 
 
 @Slf4j
+@Import(value= {AppConfig.class})
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
