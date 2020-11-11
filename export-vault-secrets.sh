c8619ca943006333e897a3db2c000764cf32b93d
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2020-11-11 17:01:43 +0100

    MMA-4953 Rettet feil config

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -12,10 +12,10 @@ then
     export AVLEVERING_SERVICEUSER_PASSWORD=$(cat /var/run/secrets/nais.io/service_user/password)
 fi
 # database
-if test -f /var/run/secrets/nais.io/database_user/password;
+if test -f /var/run/secrets/nais.io/database_user/username;
 then
     echo "Setting SPRING_DATASOURCE_USERNAME"
-    export  SPRING_DATASOURCE_PASSWORD=$(cat /var/run/secrets/nais.io/database_user/username)
+    export  SPRING_DATASOURCE_USERNAME=$(cat /var/run/secrets/nais.io/database_user/username)
 fi
 if test -f /var/run/secrets/nais.io/database_user/password;
 then
