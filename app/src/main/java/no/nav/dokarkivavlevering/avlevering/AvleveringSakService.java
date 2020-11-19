bbf15485da9b706b2a8437d73b160806750122cb
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2020-11-19 16:58:55 +0100

    MMA-4959 Beriket Sak.bruker med navn og fødselsnummer fra PDL.

--- app/src/main/java/no/nav/dokarkivavlevering/avlevering/AvleveringSakService.java
+++ app/src/main/java/no/nav/dokarkivavlevering/avlevering/AvleveringSakService.java
@@ -2,8 +2,8 @@ package no.nav.dokarkivavlevering.avlevering;
 
 import no.arkivverket.standarder.noark5.arkivstruktur.Saksmappe;
 import no.nav.dokarkivavlevering.avlevering.arkivstruktur.SaksmappeMapper;
-import no.nav.dokarkivavlevering.avlevering.consumer.pdl.PdlGraphQLConsumer;
 import no.nav.dokarkivavlevering.avlevering.domain.Sak;
+import org.apache.camel.ExchangeProperty;
 import org.apache.camel.Handler;
 import org.springframework.stereotype.Service;
 
@@ -15,21 +15,18 @@ import java.util.stream.Collectors;
  */
 @Service
 public class AvleveringSakService {
-	private final PdlGraphQLConsumer pdlGraphQLConsumer;
+	private final AvleveringSakBerikerService avleveringSakBerikerService;
 	private final SaksmappeMapper saksmappeMapper;
 
-	public AvleveringSakService(PdlGraphQLConsumer pdlGraphQLConsumer, SaksmappeMapper saksmappeMapper) {
-		this.pdlGraphQLConsumer = pdlGraphQLConsumer;
+	public AvleveringSakService(AvleveringSakBerikerService avleveringSakBerikerService, SaksmappeMapper saksmappeMapper) {
+		this.avleveringSakBerikerService = avleveringSakBerikerService;
 		this.saksmappeMapper = saksmappeMapper;
 	}
 
 	@Handler
-	public List<Saksmappe> avlevering(final List<Sak> saker) {
-		// start forretningslogikk her
-		// map til xmlstruktur
-		// hent metadata
-		// returner liste av xml objekter for marshal
-		return saker.stream().map(saksmappeMapper::map)
+	public List<Saksmappe> avlevering(final List<Sak> saker, @ExchangeProperty(AvleveringRoute.PROPERTY_TEMA) final String tema) {
+		return avleveringSakBerikerService.berikSaker(saker, tema)
+				.stream().map(saksmappeMapper::map)
 				.collect(Collectors.toList());
 	}
 }
