8cdd09514de8b9b626dc3767af745b61cd8d67bc
Joakim Borgersen <joakim.borgersen@nav.no>
2023-06-13 09:15:20 +0200

    Fix formatting av datetimes for arkivverket

--- arkivverket-schemas/src/main/java/no/nav/dokarkivavlevering/adapter/LocalDateTimeAdapter.java
+++ arkivverket-schemas/src/main/java/no/nav/dokarkivavlevering/adapter/LocalDateTimeAdapter.java
@@ -4,17 +4,19 @@ import javax.xml.bind.annotation.adapters.XmlAdapter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
 public class LocalDateTimeAdapter extends XmlAdapter<String, LocalDateTime> {
 
-	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd kk:mm:ss");
+	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 
 	@Override
 	public LocalDateTime unmarshal(String s) throws Exception {
-		return LocalDateTime.from(FORMATTER.parse(s));
+		return LocalDateTime.from(ISO_LOCAL_DATE_TIME.parse(s));
 	}
 
 	@Override
 	public String marshal(LocalDateTime localDateTime) throws Exception {
-		return FORMATTER.format(localDateTime);
+		return ISO_LOCAL_DATE_TIME.format(localDateTime);
 	}
 }
