e2905c4e3a30436aa93064fd6219f6209256c812
Joakim Borgersen <joakim.borgersen@nav.no>
2023-06-13 09:53:17 +0200

    Remove unused datetimeformatter

--- arkivverket-schemas/src/main/java/no/nav/dokarkivavlevering/adapter/LocalDateTimeAdapter.java
+++ arkivverket-schemas/src/main/java/no/nav/dokarkivavlevering/adapter/LocalDateTimeAdapter.java
@@ -2,13 +2,10 @@ package no.nav.dokarkivavlevering.adapter;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 
 public class LocalDateTimeAdapter extends XmlAdapter<String, LocalDateTime> {
-
-	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 
 	@Override
 	public LocalDateTime unmarshal(String s) throws Exception {
