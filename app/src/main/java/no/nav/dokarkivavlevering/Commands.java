5a5ff8b33a692df4371812b25e14a1eae56d0a1d
Joakim Borgersen <joakim.borgersen@nav.no>
2025-03-24 15:49:36 +0100

    Start avleverings-jobben med en command
    * Stopp avleveringsjobben fra å starte automatisk, og heller starte ved en command via gha
    * Valider at påkrevde properties er satt ved oppstart av avleveringen
    
    Co-Authored-By: avoldsund <5610775+avoldsund@users.noreply.github.com>

--- app/src/main/java/no/nav/dokarkivavlevering/Commands.java
+++ app/src/main/java/no/nav/dokarkivavlevering/Commands.java
@@ -2,30 +2,41 @@ package no.nav.dokarkivavlevering;
 
 import lombok.extern.slf4j.Slf4j;
 import no.nav.dokarkivavlevering.avlevering.ProduserAvleveringspakkeTilArkivverket;
+import no.nav.dokarkivavlevering.core.DokarkivavleveringProperties;
 import org.springframework.shell.command.annotation.Command;
-import org.springframework.shell.command.annotation.Option;
 
-import java.time.LocalDateTime;
+import static java.util.Objects.isNull;
 
 @Slf4j
 @Command(group = "arkiv")
 public class Commands {
 
 	private final ProduserAvleveringspakkeTilArkivverket produserAvleveringspakkeTilArkivverket;
+	private final DokarkivavleveringProperties dokarkivavleveringProperties;
 
-	public Commands(ProduserAvleveringspakkeTilArkivverket produserAvleveringspakkeTilArkivverket) {
+	public Commands(ProduserAvleveringspakkeTilArkivverket produserAvleveringspakkeTilArkivverket, DokarkivavleveringProperties dokarkivavleveringProperties) {
 		this.produserAvleveringspakkeTilArkivverket = produserAvleveringspakkeTilArkivverket;
+		this.dokarkivavleveringProperties = dokarkivavleveringProperties;
 	}
 
 	@Command(command = "produser-avleveringspakke-til-arkivverket",
 			description = "Produser avleveringspakke til Arkivverket.")
-	public void produserAvleveringspakkeTilArkivverket(@Option(required = true) LocalDateTime periodeStart,
-													   @Option(required = true) LocalDateTime periodeSlutt,
-													   @Option(required = true) String tema) {
+	public void produserAvleveringspakkeTilArkivverket() {
+		validerPåkrevdeProduserAvleveringspakkeTilArkivverketPropertiesErSatt();
 		log.info("produserAvleveringspakkeTilArkivverket skal produsere avleveringspakke til Arkivverket for tema={} med periodeStart={} og periodeSlutt={}",
-				tema, periodeStart, periodeSlutt);
-
+				dokarkivavleveringProperties.getTema(), dokarkivavleveringProperties.getPeriode().getStartdato(), dokarkivavleveringProperties.getPeriode().getSluttdato());
 		produserAvleveringspakkeTilArkivverket.execute();
 	}
 
+	private void validerPåkrevdeProduserAvleveringspakkeTilArkivverketPropertiesErSatt(){
+		if(isNull(dokarkivavleveringProperties.getPeriode().getStartdato())){
+			throw new MissingPropertiesException("Startdato er null");
+		}
+		if(isNull(dokarkivavleveringProperties.getPeriode().getSluttdato())){
+			throw new MissingPropertiesException("Sluttdato er null");
+		}
+		if(isNull(dokarkivavleveringProperties.getTema())){
+			throw new MissingPropertiesException("Tema er null");
+		}
+	}
 }
