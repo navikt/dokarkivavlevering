fde308f973b75777b7d892038e5a229e4c3e0f5c
Anders Opskar Voldsund <anders.voldsund@nav.no>
2025-03-24 10:29:08 +0100

    MMA-7185 Fjern unødvendig import i Application
    Co-authored-by: Joakim Borgersen <joakim.borgersen@nav.no>

--- app/src/main/java/no/nav/dokarkivavlevering/Application.java
+++ app/src/main/java/no/nav/dokarkivavlevering/Application.java
@@ -1,14 +1,10 @@
 package no.nav.dokarkivavlevering;
 
-import no.nav.dokarkivavlevering.core.CoreConfig;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.Import;
-
 
 @SpringBootApplication
-@Import(value = {CoreConfig.class})
 public class Application {
 
 	public static void main(String[] args) {
