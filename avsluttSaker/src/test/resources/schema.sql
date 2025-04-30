66d500901d5d7ba4c8e51057e9db4f7b8f9b4f04
Joakim Borgersen <joakim.borgersen@nav.no>
2025-04-30 11:35:04 +0200

    Add database-schema JOARK i itest for å speile miljø
    Co-Authored-By: avoldsund <5610775+avoldsund@users.noreply.github.com>

--- avsluttSaker/src/test/resources/schema.sql
+++ avsluttSaker/src/test/resources/schema.sql
@@ -1,39 +1,41 @@
+CREATE SCHEMA IF NOT EXISTS JOARK;
+
 -- Opprett tabell siden dette ikke er en egen JPA entitet
-create table t_saksrelasjon
+create table joark.t_saksrelasjon
 (
     sak_id         NUMBER        not null,
     feilregistrert CHAR(1),
     journalpost_id NUMBER(11, 0) not null
 );
 
-insert into t_saksrelasjon(sak_id, feilregistrert, journalpost_id)
+insert into joark.t_saksrelasjon(sak_id, feilregistrert, journalpost_id)
 VALUES (123, '0', 123456);
 
-insert into t_saksrelasjon(sak_id, feilregistrert, journalpost_id)
+insert into joark.t_saksrelasjon(sak_id, feilregistrert, journalpost_id)
 VALUES (123, '0', 123457);
 
-insert into t_saksrelasjon(sak_id, feilregistrert, journalpost_id)
+insert into joark.t_saksrelasjon(sak_id, feilregistrert, journalpost_id)
 VALUES (234, '0', 234567);
 
-insert into t_saksrelasjon(sak_id, feilregistrert, journalpost_id)
+insert into joark.t_saksrelasjon(sak_id, feilregistrert, journalpost_id)
 VALUES (345, '0', 345678);
 
-insert into t_saksrelasjon(sak_id, feilregistrert, journalpost_id)
+insert into joark.t_saksrelasjon(sak_id, feilregistrert, journalpost_id)
 VALUES (456, '1', 456789);
 
-insert into t_saksrelasjon(sak_id, feilregistrert, journalpost_id)
+insert into joark.t_saksrelasjon(sak_id, feilregistrert, journalpost_id)
 VALUES (567, '0', 5678910);
 
-insert into t_saksrelasjon(sak_id, feilregistrert, journalpost_id)
+insert into joark.t_saksrelasjon(sak_id, feilregistrert, journalpost_id)
 VALUES (678, '0', 67891011);
 
-insert into t_saksrelasjon(sak_id, feilregistrert, journalpost_id)
+insert into joark.t_saksrelasjon(sak_id, feilregistrert, journalpost_id)
 VALUES (7898, '0', 123456);
 
-insert into t_saksrelasjon(sak_id, feilregistrert, journalpost_id)
+insert into joark.t_saksrelasjon(sak_id, feilregistrert, journalpost_id)
 VALUES (7899, '0', 123456);
 
-create table t_journalpost
+create table joark.t_journalpost
 (
     journalpost_id NUMBER(11, 0) not null,
     k_journal_s    VARCHAR2(20)  not null,
@@ -42,28 +44,28 @@ create table t_journalpost
     dato_opprettet TIMESTAMP(6)
 );
 
-insert into t_journalpost(journalpost_id, k_journal_s, journalf_enhet, dato_opprettet, dato_journal)
+insert into joark.t_journalpost(journalpost_id, k_journal_s, journalf_enhet, dato_opprettet, dato_journal)
 VALUES ('123456', 'FL', '1234', '2025-01-01T13:30', '2025-01-02T13:30');
 
-insert into t_journalpost(journalpost_id, k_journal_s, journalf_enhet, dato_opprettet, dato_journal)
+insert into joark.t_journalpost(journalpost_id, k_journal_s, journalf_enhet, dato_opprettet, dato_journal)
 VALUES ('123457', 'E', '1234', '2025-01-01T13:30', '2025-01-02T13:30');
 
-insert into t_journalpost(journalpost_id, k_journal_s, journalf_enhet, dato_opprettet, dato_journal)
+insert into joark.t_journalpost(journalpost_id, k_journal_s, journalf_enhet, dato_opprettet, dato_journal)
 VALUES ('234567', 'FS', '5678', '2025-02-13T14:45', '2025-02-13T15:00');
 
-insert into t_journalpost(journalpost_id, k_journal_s, journalf_enhet, dato_opprettet, dato_journal)
+insert into joark.t_journalpost(journalpost_id, k_journal_s, journalf_enhet, dato_opprettet, dato_journal)
 VALUES ('345678', 'M', '5678', '2025-02-13T14:45', '2025-02-13T15:00');
 
-insert into t_journalpost(journalpost_id, k_journal_s, journalf_enhet, dato_opprettet, dato_journal)
+insert into joark.t_journalpost(journalpost_id, k_journal_s, journalf_enhet, dato_opprettet, dato_journal)
 VALUES ('456789', 'FS', '5678', '2025-02-13T14:45', '2025-02-13T15:00');
 
-insert into t_journalpost(journalpost_id, k_journal_s, journalf_enhet, dato_opprettet, dato_journal)
+insert into joark.t_journalpost(journalpost_id, k_journal_s, journalf_enhet, dato_opprettet, dato_journal)
 VALUES ('5678910', 'FS', '', '2025-02-13T14:45', '2025-02-13T15:00');
 
-insert into t_journalpost(journalpost_id, k_journal_s, journalf_enhet, dato_opprettet, dato_journal)
+insert into joark.t_journalpost(journalpost_id, k_journal_s, journalf_enhet, dato_opprettet, dato_journal)
 VALUES ('67891011', 'FL', '1111', '2025-01-01T13:30', '2025-01-02T13:30');
 
-create table sak
+create table joark.sak
 (
     ID                   NUMBER(10, 0) not null,
     K_SAK_STATUS         VARCHAR2(40),
@@ -81,26 +83,26 @@ create table sak
     FAGSAKNR             VARCHAR2(40)
 );
 
-insert into sak(ID, K_SAK_STATUS)
+insert into joark.sak(ID, K_SAK_STATUS)
 VALUES (123, 'AAPEN');
 
-insert into sak(ID)
+insert into joark.sak(ID)
 VALUES (234);
 
-insert into sak(ID)
+insert into joark.sak(ID)
 VALUES (345);
 
-insert into sak(ID)
+insert into joark.sak(ID)
 VALUES (456);
 
-insert into sak(ID)
+insert into joark.sak(ID)
 VALUES (568);
 
-insert into sak(ID)
+insert into joark.sak(ID)
 VALUES (678);
 
-insert into sak(ID, FAGSAKNR)
+insert into joark.sak(ID, FAGSAKNR)
 VALUES (7898, 'FAGSAK_123');
 
-insert into sak(ID, FAGSAKNR)
+insert into joark.sak(ID, FAGSAKNR)
 VALUES (7899, 'FAGSAK_234');
