3828e9bd6e608adf74d7648ec6be6bb4bbe9dfbc
Joakim Borgersen <joakim.borgersen@nav.no>
2025-05-06 15:54:08 +0200

    Pass på at saker uten fagsaknr ikke grupperes sammen
    Co-Authored-By: avoldsund <5610775+avoldsund@users.noreply.github.com>
    Co-authored-by: Joakim Borgersen <joakim.borgersen@nav.no>

--- avsluttSaker/src/main/java/no/nav/dokarkivavlevering/avsluttAlleSakerPaaTema/utils/AvsluttSakUtils.java
+++ avsluttSaker/src/main/java/no/nav/dokarkivavlevering/avsluttAlleSakerPaaTema/utils/AvsluttSakUtils.java
@@ -5,38 +5,57 @@ import no.nav.dokarkivavlevering.avsluttAlleSakerPaaTema.entities.Arbeidssak;
 import no.nav.dokarkivavlevering.avsluttAlleSakerPaaTema.repository.Arbeidsstatus;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+//TODO: Lag tester for gruppering av saker
 public class AvsluttSakUtils {
 
 	public static void oppdaterArbeidsstatusForArkivsak(Arkivsak arkivsak, Arbeidsstatus arbeidsstatus) {
-		//TODO: Ved oppdatering til en endelig status burde vi endre vanlig status også, ikke bare arbeidsstatus
 		arkivsak.arbeidssaker().forEach(arbeidssak -> arbeidssak.setArbeidsstatus(arbeidsstatus));
 	}
 
 	public static List<List<Arbeidssak>> grupperArbeidssakerPerAktoerId(List<Arbeidssak> arbeidssaker) {
-		return new ArrayList<>(arbeidssaker.stream()
+		List<Arbeidssak> arbeidssakerUtenFagsakNr = getArbeidssakerUtenFagsakNr(arbeidssaker);
+
+		Collection<List<Arbeidssak>> arbeidssakerMedFagsaknr = arbeidssaker.stream()
+				.filter(arbeidssak -> arbeidssak.getFagsaknr() != null)
 				.collect(Collectors.groupingBy(arbeidssak -> List.of(
 						arbeidssak.getAktoerId(),
 						arbeidssak.getApplikasjon(),
-						//TODO: Ikke grupper de som mangler fagsaknr
-						arbeidssak.getFagsaknr() == null ? "" : arbeidssak.getFagsaknr()
+						arbeidssak.getFagsaknr()
 				)))
-				.values()
-		);
+				.values();
+
+		ArrayList<List<Arbeidssak>> grupperteArbeidssakerPerAktoerId = new ArrayList<>();
+		arbeidssakerUtenFagsakNr.forEach(arbeidssak -> grupperteArbeidssakerPerAktoerId.add(List.of(arbeidssak)));
+		grupperteArbeidssakerPerAktoerId.addAll(arbeidssakerMedFagsaknr);
+		return grupperteArbeidssakerPerAktoerId;
 	}
 
+
 	public static List<List<Arbeidssak>> grupperArbeidssakerPerOrgnr(List<Arbeidssak> arbeidssaker) {
-		return new ArrayList<>(arbeidssaker.stream()
+		List<Arbeidssak> arbeidssakerUtenFagsakNr = getArbeidssakerUtenFagsakNr(arbeidssaker);
+
+		Collection<List<Arbeidssak>> arbeidssakermedFagsaknr = arbeidssaker.stream()
+				.filter(arbeidssak -> arbeidssak.getFagsaknr() != null)
 				.collect(Collectors.groupingBy(arbeidssak -> List.of(
 						arbeidssak.getOrgnr(),
 						arbeidssak.getApplikasjon(),
-						//TODO: Ikke grupper de som mangler fagsaknr
-						arbeidssak.getFagsaknr() == null ? "" : arbeidssak.getFagsaknr()
+						arbeidssak.getFagsaknr()
 				)))
-				.values()
-		);
+				.values();
+
+		ArrayList<List<Arbeidssak>> grupperteArbeidssakerPerOrgnr = new ArrayList<>();
+		arbeidssakerUtenFagsakNr.forEach(arbeidssak -> grupperteArbeidssakerPerOrgnr.add(List.of(arbeidssak)));
+		grupperteArbeidssakerPerOrgnr.addAll(arbeidssakermedFagsaknr);
+		return grupperteArbeidssakerPerOrgnr;
 	}
 
+	private static List<Arbeidssak> getArbeidssakerUtenFagsakNr(List<Arbeidssak> arbeidssaker) {
+		return arbeidssaker.stream()
+				.filter(arbeidssak -> arbeidssak.getFagsaknr() == null)
+				.toList();
+	}
 }
