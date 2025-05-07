5c090ba7a46450527298c9457e446dc06fafdda8
Anders Opskar Voldsund <anders.voldsund@nav.no>
2025-05-07 11:18:31 +0200

    MMA-7185 Legg på ordering på selects og ekskluder ferdige statusar
    Co-authored-by: Joakim Borgersen <joakim.borgersen@nav.no>

--- avsluttSaker/src/test/java/no/nav/dokarkivavlevering/avsluttAlleSakerPaaTema/ArbeidssakRepositoryTest.java
+++ avsluttSaker/src/test/java/no/nav/dokarkivavlevering/avsluttAlleSakerPaaTema/ArbeidssakRepositoryTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
+import static no.nav.dokarkivavlevering.avsluttAlleSakerPaaTema.repository.Arbeidsstatus.ENDELIGE_STATUSER;
+import static no.nav.dokarkivavlevering.avsluttAlleSakerPaaTema.repository.Arbeidsstatus.FERDIG_SAK_AVSLUTTET;
+import static no.nav.dokarkivavlevering.avsluttAlleSakerPaaTema.repository.Arbeidsstatus.HENTET_FRA_PDL;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ArbeidssakRepositoryTest extends AbstractRepositoryTest {
@@ -16,20 +19,23 @@ class ArbeidssakRepositoryTest extends AbstractRepositoryTest {
 				baseArkivsakForAktoerId().sakId(123L).build(),
 				baseArkivsakForAktoerId().sakId(234L).build(),
 				baseArkivsakForAktoerId().sakId(345L).build()));
-		List<Long> sakIds = arbeidssakRepository.findAllSakIdsWhereStatusIsNullOrAapen();
+		List<Long> sakIds = arbeidssakRepository.findAllSakIdsWhereStatusIsNullOrAapen(ENDELIGE_STATUSER);
 
 		assertThat(sakIds).isEqualTo(List.of(123L, 234L, 345L));
 	}
 
 	@Test
-	void skalKunHenteSakIdHvorSakStatusErNullEllerAapen() {
+	void skalKunHenteSakIdHvorSakStatusErNullEllerAapenOgArbeidsstatusIkkeErEndelig() {
 		arbeidssakRepository.saveAll(List.of(
 				baseArkivsakForAktoerId().sakId(1L).status("AAPEN").build(),
 				baseArkivsakForAktoerId().sakId(2L).build(),
-				baseArkivsakForAktoerId().sakId(3L).status("BAD_STATUS").build()));
+				baseArkivsakForAktoerId().sakId(3L).status("BAD_STATUS").build(),
+				baseArkivsakForAktoerId().sakId(4L).arbeidsstatus(HENTET_FRA_PDL).build(),
+				baseArkivsakForAktoerId().sakId(5L).arbeidsstatus(FERDIG_SAK_AVSLUTTET).build()
+		));
 
-		List<Long> sakIds = arbeidssakRepository.findAllSakIdsWhereStatusIsNullOrAapen();
+		List<Long> sakIds = arbeidssakRepository.findAllSakIdsWhereStatusIsNullOrAapen(ENDELIGE_STATUSER);
 
-		assertThat(sakIds).isEqualTo(List.of(1L, 2L));
+		assertThat(sakIds).isEqualTo(List.of(1L, 2L, 4L));
 	}
 }
