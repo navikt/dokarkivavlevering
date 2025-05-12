a3ed5cc7fe92d66af71912a1f5a10ff86bf33120
Anders Opskar Voldsund <anders.voldsund@nav.no>
2025-05-12 15:36:19 +0200

    MMA-7185 Fjern trunkering av administrativEnhet-feltet
    Kolonnene administrativ_enhet og sak_ansvarlig på sak-tabellen er utvida slik at input > 40 teikn skal gå bra.

--- avsluttSaker/src/main/java/no/nav/dokarkivavlevering/avsluttAlleSakerPaaTema/services/AvsluttAlleSakerDoUpdates.java
+++ avsluttSaker/src/main/java/no/nav/dokarkivavlevering/avsluttAlleSakerPaaTema/services/AvsluttAlleSakerDoUpdates.java
@@ -92,11 +92,7 @@ public class AvsluttAlleSakerDoUpdates {
 			Journalpost eldsteJournalpost = journalpostService.finnEldsteJournalpostForArkivsak(arkivsak);
 			LocalDateTime datoSakOpprettet = eldsteJournalpost.getOpprettetdato();
 			String administrativEnhet = bestemAdministrativEnhet(eldsteJournalpost, arkivsak);
-			//TODO: Expand column! !! !!! !!!! !!!!! !!!!!!! !!!!!!!! !!!!!!!!!!
-			if (administrativEnhet.length() > 40) {
-				log.info("Mottok for lang Administrativ Enhet: " + administrativEnhet + " Med lengde: " + administrativEnhet.length());
-				administrativEnhet = administrativEnhet.substring(0, 40);
-			}
+
 			avsluttSakRepository.avsluttSaker(arkivsak.getArbeidssaksIder(), hentDatoAvsluttet(), datoSakOpprettet, administrativEnhet);
 			oppdaterArbeidsstatusForArkivsak(arkivsak, FERDIG_SAK_AVSLUTTET);
 			log.info("Har avsluttet arkivsak med saksIder={}", arkivsak.getArbeidssaksIder());
