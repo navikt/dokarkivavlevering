cb42e89142ac9f397f3253313029b39c5b099e92
Joakim Borgersen <joakim.borgersen@nav.no>
2025-05-21 13:21:48 +0200

    Avslutt jobben etter den har kjørt ferdig

--- avsluttSaker/src/main/java/no/nav/dokarkivavlevering/avsluttAlleSakerPaaTema/AvsluttAlleSakerPaaTema.java
+++ avsluttSaker/src/main/java/no/nav/dokarkivavlevering/avsluttAlleSakerPaaTema/AvsluttAlleSakerPaaTema.java
@@ -2,6 +2,8 @@ package no.nav.dokarkivavlevering.avsluttAlleSakerPaaTema;
 
 import lombok.extern.slf4j.Slf4j;
 import no.nav.dokarkivavlevering.avsluttAlleSakerPaaTema.services.AvsluttAlleSakerService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -17,10 +19,12 @@ public class AvsluttAlleSakerPaaTema {
 
 	private final AvsluttSakProperties avsluttSakProperties;
 	private final AvsluttAlleSakerService avsluttAlleSakerService;
+	private final ApplicationContext context;
 
-	public AvsluttAlleSakerPaaTema(AvsluttSakProperties avsluttSakProperties, AvsluttAlleSakerService avsluttAlleSakerService) {
+	public AvsluttAlleSakerPaaTema(AvsluttSakProperties avsluttSakProperties, AvsluttAlleSakerService avsluttAlleSakerService, ApplicationContext context) {
 		this.avsluttSakProperties = avsluttSakProperties;
 		this.avsluttAlleSakerService = avsluttAlleSakerService;
+		this.context = context;
 	}
 
 	@Scheduled(initialDelay = 1000)
@@ -35,5 +39,6 @@ public class AvsluttAlleSakerPaaTema {
 		avsluttAlleSakerService.avsluttAlleSaker();
 
 		log.info("avsluttAlleSakerPaaTema har avsluttet alle saker i arbeidstabellen med referanse={}, avsluttetDato={} og administrativEnhet={}", referanse, avsluttetDato, administrativEnhet);
+		System.exit(SpringApplication.exit(context, () -> 0));
 	}
 }
